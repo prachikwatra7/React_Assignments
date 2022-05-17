c88c19412db9223429c6661012ded81363728e67
prachikwatra7 <prachikwatra7@gmail.com>
2022-05-17 17:56:30 +0530

    Added hook assignments

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 24,
+    message: "random message",
+  });
+  const handleMessage = () => {
+    setPerson({ ...person, message: "hello world" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={handleMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
